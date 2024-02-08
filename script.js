2b9a9b3ab44639119705a47b3b2743a9d97946a9
dylanchen1 <dylanchen1@kpmg.co.nz>
2024-02-08 16:29:04 +1300

    add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// instantiate variables
 const grid = document.querySelector('.grid');
 let gridWidth = grid.offsetWidth;
 let gridHeight = grid.offsetHeight;
@@ -6,6 +7,7 @@ let number  = 16;
 let cellWidth = gridWidth / number;
 let cellHeight = gridHeight / number;
 
+// create the initial grid
 for (let i = 0; i < number; i++) {
     let row = document.createElement('div');
     grid.appendChild(row);
@@ -18,13 +20,14 @@ for (let i = 0; i < number; i++) {
     }
 }
 
+// implement drawing with mouse listeners
 let isDown = false;
 const gridCells = document.querySelectorAll('.cell');
     
 gridCells.forEach(cell => {
     cell.addEventListener('mousedown', (e) => {
         isDown = true;
-        e.preventDefault();
+        e.preventDefault(); // so we can still draw when we start from a drawn cell
     });
     cell.addEventListener('mouseover', () => {
         if (isDown) { 
@@ -37,12 +40,16 @@ gridCells.forEach(cell => {
     });
 });
 
+// create button logic
 let button = document.querySelector('button');
+
 button.addEventListener('click', () => {
     let originalNumber = number;
+
     let numberPrompt = prompt('Enter the number of boxes to be generated on a side of the grid (min 4, max 100)', 16);
     if (numberPrompt !== null) {
         number = parseInt(numberPrompt);
+        // check if number entered is in valid range, otherwise keep prompting or cancel
         while (number > 100 || number < 4) {
                 alert('Not a valid number! Please enter a number between 4 and 100!')
                 numberPrompt = prompt('Enter the number of boxes to be generated on a side of the grid (min 4, max 100)', 16);
@@ -54,6 +61,7 @@ button.addEventListener('click', () => {
             }
         }
         
+        // if number is valid redraw grid, if cancelled then continue with number set in previous prompt
         if (numberPrompt !== null) {
             grid.innerHTML = "";
             gridWidth = grid.offsetWidth;
@@ -64,7 +72,6 @@ button.addEventListener('click', () => {
             
             for (let i = 0; i < number; i++) {
                 let row = document.createElement('div');
-                row.style.width = ``
                 grid.appendChild(row);
                 for (let j = 0; j < number; j++) {
                 const cell = document.createElement('div');
@@ -74,6 +81,7 @@ button.addEventListener('click', () => {
                 row.appendChild(cell);
                 }
             }
+            
             const gridCells = document.querySelectorAll('.cell');
             gridCells.forEach(cell => {
                 cell.addEventListener('mousedown', (e) => {
