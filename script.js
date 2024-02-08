612cb85c2a58b034c0b764e67a1e934033e0d862
dylanchen1 <dylanchen1@kpmg.co.nz>
2024-02-08 13:11:50 +1300

    add grid size check

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ const grid = document.querySelector('.grid');
 
 let button = document.querySelector('button');
 button.addEventListener('click', () => {
-    let number = prompt('Enter the number of boxes to be generated on a side of the grid', 16);
+    let number = prompt('Enter the number of boxes to be generated on a side of the grid (min 4, max 100)', 16);
     number = parseInt(number); 
+    while (number > 100 || number < 4) {
+        alert('Not a valid number! Please enter a number between 4 and 100!')
+        number = prompt('Enter the number of boxes to be generated on a side of the grid (min 4, max 100)', 16);
+        if (number === null) {
+            break;
+        }
+    }
     grid.innerHTML = "";
     let gridWidth = grid.offsetWidth;
     let gridHeight = grid.offsetHeight;
@@ -16,10 +23,7 @@ button.addEventListener('click', () => {
     
     for (let i = 0; i < number; i++) {
         let row = document.createElement('div');
-        // row.style.width = `${cellWidth}ox`;
-        row.style.height = `${cellHeight}px`;
         grid.appendChild(row);
-        // const row = grid.appendChild(document.createElement('div'));
         for (let j = 0; j < number; j++) {
            const cell = document.createElement('div');
            cell.classList.add('cell');
@@ -28,7 +32,7 @@ button.addEventListener('click', () => {
            row.appendChild(cell);
         }
     }
-})
+});
 
 // let gridWidth = grid.offsetWidth;
 // let gridHeight = grid.offsetHeight;
