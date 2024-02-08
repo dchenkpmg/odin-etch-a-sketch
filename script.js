e0e949420e939a6bfc5a3fc26d645f49f9e50845
dylanchen1 <dylanchen1@kpmg.co.nz>
2024-02-08 15:42:36 +1300

    add drawing function

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let number  = 16;
 let cellWidth = gridWidth / number;
 let cellHeight = gridHeight / number;
 
-
 for (let i = 0; i < number; i++) {
     let row = document.createElement('div');
     grid.appendChild(row);
@@ -19,6 +18,23 @@ for (let i = 0; i < number; i++) {
     }
 }
 
+const gridCells = document.querySelectorAll('.cell');
+    
+gridCells.forEach(cell => {
+    cell.addEventListener('mousedown', (e) => {
+        isDown = true;
+        e.preventDefault();
+    });
+    cell.addEventListener('mouseover', () => {
+        if (isDown) { 
+            cell.style.backgroundColor = 'blue';
+        }
+    });
+    cell.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+});
+
 let button = document.querySelector('button');
 button.addEventListener('click', () => {
     let originalNumber = number;
@@ -26,60 +42,50 @@ button.addEventListener('click', () => {
     if (numberPrompt !== null) {
         number = parseInt(numberPrompt);
         while (number > 100 || number < 4) {
-            alert('Not a valid number! Please enter a number between 4 and 100!')
-            numberPrompt = prompt('Enter the number of boxes to be generated on a side of the grid (min 4, max 100)', 16);
-            console.log(numberPrompt)
-            if (numberPrompt === null) {
-                number = originalNumber;
-                break;
-            }
-            number = parseInt(numberPrompt);
-            // if (number === null) {
-            //     break;
-            // }
-        }
-    }
-    
-    if (number !== null) {
-        grid.innerHTML = "";
-        gridWidth = grid.offsetWidth;
-        gridHeight = grid.offsetHeight;
-        
-        cellWidth = gridWidth / number;
-        cellHeight = gridHeight / number;
-        
-        for (let i = 0; i < number; i++) {
-            let row = document.createElement('div');
-            row.style.width = ``
-            grid.appendChild(row);
-            for (let j = 0; j < number; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.style.width = `${cellWidth}px`;
-            cell.style.height = `${cellHeight}px`;
-            row.appendChild(cell);
+                alert('Not a valid number! Please enter a number between 4 and 100!')
+                numberPrompt = prompt('Enter the number of boxes to be generated on a side of the grid (min 4, max 100)', 16);
+                if (numberPrompt === null) {
+                    number = originalNumber;
+                    break;
+                }
+                number = parseInt(numberPrompt);
             }
         }
-    }
-});
-
-// let gridWidth = grid.offsetWidth;
-// let gridHeight = grid.offsetHeight;
-
-// let cellWidth = gridWidth / number;
-// let cellHeight = gridHeight / number;
-
-// for (let i = 0; i < number * number; i++) {
-//     let row = document.createElement('div');
-//     // row.style.width = `${cellWidth}ox`;
-//     row.style.height = `${cellHeight}px`;
-//     grid.appendChild(row);
-//     // const row = grid.appendChild(document.createElement('div'));
-//     for (let j = 0; j < number * number; j++) {
-//        const cell = document.createElement('div');
-//        cell.classList.add('cell');
-//        cell.style.width = `${cellWidth}px`;
-//        cell.style.height = `${cellHeight}px`;
-//        row.appendChild(cell);
-//     }
-// }
+        
+        if (numberPrompt !== null) {
+            grid.innerHTML = "";
+            gridWidth = grid.offsetWidth;
+            gridHeight = grid.offsetHeight;
+            
+            cellWidth = gridWidth / number;
+            cellHeight = gridHeight / number;
+            
+            for (let i = 0; i < number; i++) {
+                let row = document.createElement('div');
+                row.style.width = ``
+                grid.appendChild(row);
+                for (let j = 0; j < number; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.style.width = `${cellWidth}px`;
+                cell.style.height = `${cellHeight}px`;
+                row.appendChild(cell);
+                }
+            }
+            const gridCells = document.querySelectorAll('.cell');
+            gridCells.forEach(cell => {
+                cell.addEventListener('mousedown', (e) => {
+                    isDown = true;
+                    e.preventDefault();
+                });
+                cell.addEventListener('mouseover', () => {
+                    if (isDown) { 
+                        cell.style.backgroundColor = 'blue';
+                    }
+                });
+                cell.addEventListener('mouseup', () => {
+                    isDown = false;
+                });
+            });
+        }
+    });
